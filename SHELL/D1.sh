df2e09f5103ec8f32916389a85c025086a2b265e
lyeryne <lyeryne@gmail.com>
2023-12-21 23:41:45 +0100

    Louaye/Clem_21/12_23:41

--- SHELL/D1.sh
+++ SHELL/D1.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+if [ $# -ne 1 ] ; then
+    echo "Tu dois utiliser qu'un argument" >&2
+    exit 1
+   
+    elif [ $1 -ne 'data.csv' ] || [ ! -f $1 ] || [ ! -e $1 ]; then #question : est ce que le fichier .csv se nommera data.csv
+        echo "Ce n'est pas le fichier qu'on attend" >&2
+        exit 2
+fi
+
+#question : Pourquoi utiliser cut si on a AWK
+
+colonne
+
