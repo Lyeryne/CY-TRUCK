9c4022cde35c84754a91efeceba79c3fc4403604
lyeryne <lyeryne@gmail.com>
2023-12-22 23:08:05 +0100

    Louaye_22/12_23:07

--- SHELL/D1.sh
+++ SHELL/D1.sh
@@ -3,8 +3,6 @@
 if [ $# -ne 1 ] ; then
     echo "Tu dois utiliser qu'un argument" >&2
     exit 1
-<<<<<<< HEAD
-   
     elif [ $1 -ne 'data.csv' ] || [ ! -f $1 ] || [ ! -e $1 ]; then #question : est ce que le fichier .csv se nommera data.csv
         echo "Ce n'est pas le fichier qu'on attend" >&2
         exit 2
@@ -14,12 +12,3 @@ fi
 
 colonne
 
-=======
-elif [ $1 -ne "data.csv" ] || [ ! -f $1 ] || [ ! -e $1 ]; then
-    echo "Ce n'est pas le fichier qu'on attend" >&2
-    exit 2
-fi
-
-trajets=`awk -F';' '{if(NR>1) count[$6]++} END {for (person in count) print person, count[person]}' data.csv`
-echo "$trajets"
->>>>>>> 71e112d (oui)
