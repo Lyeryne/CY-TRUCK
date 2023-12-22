d26c37fc73980eea4da658a331ad81613e9dff73
lyeryne <lyeryne@gmail.com>
2023-12-22 23:11:12 +0100

    Louaye_22/12_23:11

--- SHELL/D1.sh
+++ SHELL/D1.sh
@@ -3,12 +3,11 @@
 if [ $# -ne 1 ] ; then
     echo "Tu dois utiliser qu'un argument" >&2
     exit 1
-    elif [ $1 -ne 'data.csv' ] || [ ! -f $1 ] || [ ! -e $1 ]; then #question : est ce que le fichier .csv se nommera data.csv
-        echo "Ce n'est pas le fichier qu'on attend" >&2
-        exit 2
+elif [ $1 -ne "data.csv" ] || [ ! -f $1 ] || [ ! -e $1 ]; then
+    echo "Ce n'est pas le fichier qu'on attend" >&2
+    exit 2
 fi
 
-#question : Pourquoi utiliser cut si on a AWK
-
-colonne
+trajets=`awk -F';' '{if(NR>1) count[$6]++} END {for (person in count) print person, count[person]}' data.csv`
+echo "$trajets"
 
