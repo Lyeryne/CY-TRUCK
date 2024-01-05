8d05675d362051c16e1fb559469d48c8077f2bd5
lyeryne <lyeryne@gmail.com>
2024-01-05 17:13:50 +0100

    Louaye_05/01_17:13

--- L.sh
+++ L.sh
@@ -12,8 +12,9 @@ CPID=$! # Cela prend le PID du processus ($!) qui vient d'être mis en arrière-
 # - la distance totale de chaque trajet ($1) 
 # en ordonnant le résultat par numéro d’identifiant ($5) de trajet croissant
 # - (NR>1) exclut la première ligne et (LC_NUMERIC=C) evite les problèmes de formatage numérateur lors de l'executable de la commande dans un script.
-distance_trajet=$(LC_NUMERIC=C awk -F';' 'NR>1 {distance[$1] += $5} END {for (conducteur in distance) printf "%s;%.6f\n", conducteur, distance[conducteur]}' "data/$1" | sort -t';' -k2 -r -n | head -10 | sort -t';' -k1 -n -r) 
+distance_trajet=$(LC_NUMERIC=en_US.UTF-8  awk -F';' 'NR>1 {distance[$1] += $5} END {for (conducteur in distance) printf "%s;%.6f\n", conducteur, distance[conducteur]}' "data/$1" | sort -t';' -k2 -r -n | head -10 | sort -t';' -k1 -n -r) 
 
+#cut -d';' -f1,5
 # Envoie des résultats d’exécutions précédentes(echo) dans le dossier 'temp' 
 echo "$distance_trajet" > temp/gnu_data_L.txt 
 
@@ -29,7 +30,7 @@ fi
 
 # Affiche que le temps (time -p 'la ligne de code') puis on dirige la sortie d'erreur standard (2>) au même endroit que la sortie standard (&1) 
 # puis on redirige la sortie vers awk pour extraire le temps "real" de la sortie de time afin de le stocker dans le fichier txt
-t=$( { time -p LC_NUMERIC=C awk -F';' 'NR>1 {distance[$1] += $5} END {for (conducteur in distance) printf "%s;%.6f\n", conducteur, distance[conducteur]}' "data/$1" | sort -t';' -k2 -r -n | head -10 | sort -t';' -k1 -n -r; } 2>&1 | awk '/^real/ {print $2}' )
+t=$( { time -p LC_NUMERIC=en_US.UTF-8  awk -F';' 'NR>1 {distance[$1] += $5} END {for (conducteur in distance) printf "%s;%.6f\n", conducteur, distance[conducteur]}' "data/$1" | sort -t';' -k2 -r -n | head -10 | sort -t';' -k1 -n -r; } 2>&1 | awk '/^real/ {print $2}' )
 
 # Ligne de code : cela enverra le signal SIGUSR1(il se trouve dans Affichage_Temps.sh) au processus identifié par le PID stocké dans la variable $CPID, 
 # déclenchant ainsi la fonction stop_compteur dans le script.
